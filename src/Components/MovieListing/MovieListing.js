5023df7b7c4dccfe4a592da7711eaa13b4ea4332
Rounak Neogy <56738141+RounakNeogy@users.noreply.github.com>
2022-07-26 00:43:33 +0530

    Update MovieListing.js

--- src/Components/MovieListing/MovieListing.js
+++ src/Components/MovieListing/MovieListing.js
@@ -1,39 +1,68 @@
 import React from 'react';
 import Slider from 'react-slick';
 import { useSelector } from 'react-redux';
-import { getAllMovies,getAllShows } from '../../Features/movies/movieSlice';
+import { getAllMovies, getAllShows } from '../../Features/movies/movieSlice';
 import MovieCard from '../MovieCard/MovieCard.js';
 import "./MovieListing.scss";
-import {settings} from "../../Common/settings.js";
+import { settings } from "../../Common/settings.js";
+import { getStatus } from '../../Features/movies/movieSlice';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function MovieListing() {
-    
+
 
     const movies = useSelector(getAllMovies);
     const shows = useSelector(getAllShows);
 
-    let renderMovies = "",renderShows="";
+    let renderMovies = "", renderShows = "";
 
-    // console.log(movies);
-    renderMovies = (movies.Response === "True") ? (
-        movies.Search.map((movie, index) => (
+    console.log(shows);
+    if (movies.Response === "True"){
+        renderMovies = movies.Search.map((movie, index) => (
             <MovieCard key={index} data={movie} />
         ))
-    ) : (
-        <div className="movies-error">
-            <h3>{movies.Error}</h3>
-        </div>
-    )
+    }
+    else if(movies>=500 && movies<600){
+        return (
+            <div className="movies-server-error">
+                <h3>Internet Server Error</h3>
+            </div>
+        )
+    }
+    else if(movies===401 || movies===403){
+        return <PageNotFound/>;
+    }
+    else{
+        return(
+            <div className='movies-internet-error'>
+                <h1>Check Internet connection</h1>
+            </div>
+        )
+    }
 
-    renderShows = (shows.Response === "True") ? (
-        shows.Search.map((show, index) => (
-            <MovieCard key={index} data={show} />
+
+    if (shows.Response === "True"){
+        renderShows = shows.Search.map((movie, index) => (
+            <MovieCard key={index} data={movie} />
         ))
-    ) : (
-        <div className="movies-error">
-            <h3>{movies.Error}</h3>
-        </div>
-    )
+    }
+    else if(shows>=500 && shows<600){
+        return (
+            <div className="movies-server-error">
+                <h3>Internet Server Error</h3>
+            </div>
+        )
+    }
+    else if(shows===401 || shows===403){
+        return <PageNotFound/>;
+    }
+    else{
+        return(
+            <div className='movies-internet-error'>
+                <h1>Check Internet connection</h1>
+            </div>
+        )
+    }
 
     return (
         <div className='movie-wrapper'>
